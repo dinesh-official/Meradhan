e03f11c251515a6f61ee62743e0af2bbaa48ac75
Morningstar005 <hemantbhatnagar0005@gmail.com>
2025-10-14 19:04:48 +0530

    Refactor API structure and add CRM user management functionality

--- packages/apiGateway/src/types/response.types.ts
+++ packages/apiGateway/src/types/response.types.ts
@@ -1,36 +1,79 @@
 import type { ROLES } from "../core/constants/role";
 
-type Role = typeof ROLES[number];
+type Role = (typeof ROLES)[number];
 
-export type BaseResponseData<T =  undefined> = {
-    statusCode: number;
-    success: boolean;
-    message: string;
-    responseData: T;
+export type BaseResponseData<T = undefined> = {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  responseData: T;
 };
-
 // auth/login-with-otp
 export type LoginWithOtpDataResponse = BaseResponseData<{
-    token: string
+  token: string;
 }>;
-
 // auth/verify-otp
 export type OtpVerifyDataResponse = BaseResponseData<{
-    token: string;
-    id: number;
-    email: string;
-    name: string;
-    phoneNo: string;
-    avatar: string;
-    role: Role;
+  token: string;
+  id: number;
+  email: string;
+  name: string;
+  phoneNo: string;
+  avatar: string;
+  role: Role;
 }>;
-
 // /session
 export type UserSessionDataResponse = BaseResponseData<{
-    id: number;
-    email: string;
-    name: string;
-    phoneNo: string;
-    avatar: string;
-    role: Role;
+  id: number;
+  email: string;
+  name: string;
+  phoneNo: string;
+  avatar: string;
+  role: Role;
+}>;
+
+// schema of get,create and update crm api of data users profile
+export type CrmUserBase = {
+  id: number;
+  name: string;
+  email: string;
+  phoneNo: string;
+  avatar: string;
+  lastLogin: string | null;
+  role: Role;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: number;
+};
+
+export type CrmUserAccountStatus ="SUSPENDED"| "ACTIVE";
+
+export type CrmUsersProfile = CrmUserBase & {
+  accountStatus: CrmUserAccountStatus;
+  createdBy: number | null;
+};
+
+export type PaginationMeta = {
+  total: number;
+  page: number;
+  pageSize: number;
+  totalPages: number;
+};
+
+// patch crm/users/:id
+export type UpdateUserResponse = BaseResponseData<CrmUserBase>;
+
+// post crm/users
+export type CreateUsersResponse = BaseResponseData<CrmUserBase>;
+
+// GET crm/users/:id
+export type UserByIdResponse = BaseResponseData<CrmUserBase>;
+
+// delete crm/users/:id
+export type DeleteUserResponse = BaseResponseData<boolean>;
+
+// GET /crm/users (paginated list)
+export type FindManyUsersResponse = BaseResponseData<{
+  data: CrmUsersProfile[];
+  meta: PaginationMeta;
 }>;
