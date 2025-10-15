b6646e6b2456cc7979f82e21d358988b52f38290
Sourav <sourav0w@gmail.com>
2025-10-16 02:08:59 +0530

    fix: Correct request limit duration and improve error message in OTP login limiter

--- backend/src/resource/auth/auth.limit.ts
+++ backend/src/resource/auth/auth.limit.ts
@@ -3,16 +3,16 @@ import type { Request, Response } from 'express';
 import { rateLimit, type RateLimitRequestHandler } from 'express-rate-limit';
 
 export const loginWithOtpLimiter: RateLimitRequestHandler = rateLimit({
-    windowMs: 10 * 60 * 1000, // 15 minutes
-    limit: 10,
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 10,
     standardHeaders: 'draft-8',
     legacyHeaders: false,
     ipv6Subnet: 56,
     handler: (req: Request, res: Response) => {
-        res.sendResponse({
+        res.status(HttpStatus.TOO_MANY_REQUESTS).json({
             success: false,
             statusCode: HttpStatus.TOO_MANY_REQUESTS,
-            message: 'You have exceeded the request limit. Please try again after some times.',
+            message: 'You have exceeded the request limit. Please try again later.',
             responseData: {
                 code: 'TOO_MANY_REQUESTS',
                 message: 'You have exceeded the request limit. Please try again later.',
