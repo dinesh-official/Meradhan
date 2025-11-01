a1e703f7f78fd0b7a0233c6f72c9b06b65a1d095
Morningstar005 <hemantbhatnagar0005@gmail.com>
2025-11-01 12:22:28 +0530

    bondformlogic complete

---
+++ frontend/meradhan/src/app/(pages)/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import ViewPort from '@/global/components/wrapper/ViewPort'
+import React from 'react'
+
+const page = () => {
+  return (
+    <ViewPort>page</ViewPort>
+  )
+}
+
+export default page
