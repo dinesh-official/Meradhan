2f82cb159ed11902eed65445f60f61411e28506f
Sourav Bapari <sourav0w@gmail.com>
2025-11-23 03:22:56 +0530

    update

--- frontend/meradhan/src/core/config/base.urls.ts
+++ frontend/meradhan/src/core/config/base.urls.ts
@@ -11,7 +11,7 @@ export const BASES = {
   production: {
     HOST: "https://test.meradhan.co",
     API_SERVER: "https://testapi.meradhan.co",
-    API_BACKEND_URL_IP: "http://34.131.27.101:4000",
+    API_BACKEND_URL_IP: "http://34.47.136.227:4000",
     CMS: "https://spydertest.meradhan.co",
     ASSETS: "https://jfhfryiyfqrytbtzsdtj.supabase.co/storage/v1/object/public",
   },
