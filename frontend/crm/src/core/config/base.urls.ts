640ba41aefbfb271e5162e18f191c91b1ee28701
Sourav Bapari <sourav0w@gmail.com>
2025-11-23 00:54:06 +0530

    fix: update CMS and API URLs in base configuration for production environment

--- frontend/crm/src/core/config/base.urls.ts
+++ frontend/crm/src/core/config/base.urls.ts
@@ -3,13 +3,13 @@ export const BASES = {
   development: {
     HOST: "http://localhost:3000",
     API_SERVER: "http://localhost:4000",
-    CMS: process.env.STRAPI_HOST_URL || "",
+    CMS: "https://spydertest.meradhan.co",
     API_BACKEND_URL_IP: "http://localhost:4000",
     ASSETS: "https://jfhfryiyfqrytbtzsdtj.supabase.co/storage/v1/object/public",
   },
   production: {
-    HOST: "https://test.meradhan.co",
-    API_SERVER: "https://testapi.meradhan.co",
+    HOST: "https://meradhan.co",
+    API_SERVER: "https://api.meradhan.co",
     API_BACKEND_URL_IP: "http://34.131.27.101:4000",
     CMS: "https://spydertest.meradhan.co",
     ASSETS: "https://jfhfryiyfqrytbtzsdtj.supabase.co/storage/v1/object/public",
