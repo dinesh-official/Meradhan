8b24ffe313f41dd9776bf7b50bd335dcc5c6d143
Sourav Bapari <hey.sourav.work@gmail.com>
2025-12-10 00:11:21 +0530

    fix config

--- frontend/crm/src/core/config/base.urls.ts
+++ frontend/crm/src/core/config/base.urls.ts
@@ -1,6 +1,6 @@
 // Centralized base URLs for all environments
 export const BASES_URLS = {
-  HOST: process.env.NEXT_PUBLIC_HOST_URL,
+  HOST: process.env.NEXT_PUBLIC_CRM_HOST_URL,
   API_SERVER: process.env.NEXT_PUBLIC_BACKEND_HOST_URL,
   API_BACKEND_URL_IP: process.env.NEXT_PUBLIC_BACKEND_IP_URL,
   CMS: process.env.NEXT_PUBLIC_STRAPI_HOST_URL,
