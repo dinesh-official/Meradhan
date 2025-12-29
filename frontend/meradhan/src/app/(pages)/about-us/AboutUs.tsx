b3d994eacd88e5c4609ebe3a3e50bd79bd9b54c1
Sourav Bapari <hey.sourav.work@gmail.com>
2025-12-30 01:31:03 +0530

    remove import

--- frontend/meradhan/src/app/(pages)/about-us/AboutUs.tsx
+++ frontend/meradhan/src/app/(pages)/about-us/AboutUs.tsx
@@ -1,9 +1,8 @@
 import TopTitleDesc from "@/global/components/basic/TopTitleDesc";
-import { sanitizeStrapiHTML } from "@/global/utils/html-sanitizer";
 import { T_PAGE_DATA } from "@/graphql/pagesGQLAction";
 import AboutMeraDhanSection from "./_components/sections/AboutMeraDhanSection";
-import MissionVisionSection from "./_components/sections/MissionVisionSection";
 import CoreValuesSection from "./_components/sections/CoreValuesSection";
+import MissionVisionSection from "./_components/sections/MissionVisionSection";
 import OfferingsSection from "./_components/sections/OfferingsSection";
 import OurExports from "./_components/sections/OurExparts";
 import WhyChooseUsSection from "./_components/sections/WhyChooseUsSection.tsx";
