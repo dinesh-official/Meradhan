13f907b75b734e3e4426460815a1c6796a7b3eb0
Sourav Bapari <hey.sourav.work@gmail.com>
2025-12-31 01:03:27 +0530

    checkIsKraMatched

--- backend/src/jobs/kra_worker/CheckKraStatus.ts
+++ backend/src/jobs/kra_worker/CheckKraStatus.ts
@@ -9,6 +9,8 @@ import { KraProcess } from "./KraWorker.service";
 export const checkKraProcessCheckStatus = (response: T_APP_PAN_INQ) => {
   const status =
     response?.APP_RES_ROOT?.APP_PAN_INQ?.APP_STATUS?.toLowerCase().trim();
+  const updtStatus =
+    response?.APP_RES_ROOT?.APP_PAN_INQ?.APP_UPDT_STATUS?.toLowerCase().trim();
 
   if (!status) {
     if (!response?.APP_RES_ROOT?.APP_PAN_INQ.ERROR?.trim()) {
@@ -27,26 +29,29 @@ export const checkKraProcessCheckStatus = (response: T_APP_PAN_INQ) => {
     return "REGISTER";
   }
 
-  // SUCCESS -
-  if (
-    status?.startsWith("kyc registd") ||
-    status?.startsWith("kyc validated")
-  ) {
-    return "PASS";
-  }
-
   // PENDING -
   if (
     status?.includes("underprocess") ||
     status?.includes("onhold") ||
-    status?.includes("incomplete")
+    status?.includes("incomplete") ||
+    updtStatus?.includes("underprocess") ||
+    updtStatus?.includes("onhold") ||
+    updtStatus?.includes("incomplete")
   ) {
     return "WAITING";
   }
 
   // FAILED (explicit) -
   if (status?.includes("rejted") || status?.includes("rejected")) {
-    return "PASS";
+    return "AVAILABLE";
+  }
+
+  // SUCCESS -
+  if (
+    status?.startsWith("kyc registd") ||
+    status?.startsWith("kyc validated")
+  ) {
+    return "AVAILABLE";
   }
 
   // Default fallback -
