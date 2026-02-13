1fa4980a9ad474dcb13a7e1f52c9b7a503ab04ba
Sourav Bapari <hey.sourav.work@gmail.com>
2026-02-13 19:49:57 +0530

    Update backend routes and enhance participant management: add nseWebhookRoutes and dashboardRoutes, reorganize import statements, and implement unregistered participant functionality in test script.

--- backend/test.ts
+++ backend/test.ts
@@ -1,42 +1,51 @@
-import { addKraWorkerJob } from "@jobs/kra_worker/kraWroker.helper";
-import { cacheStorage } from "@store/redis_store";
+import { NseCBRICS } from "@modules/RFQ/nse/nse_CBRICS";
+import { ParticipantManager } from "@services/refq/nse/cbrics_manager.service";
+import type { AxiosError } from "axios";
 
-const user = {
-  "userId": 66,
-  "kycId": 431,
-};
-
-const delay = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
-
+const participant = new ParticipantManager();
 const main = async () => {
   try {
-    const cacheKey = `KRA:${user.userId}-${user.kycId}`;
-    // Set new cache state
-    await cacheStorage.set(cacheKey, "MODIFY", 72 * 60 * 60);
-
-    console.log(`Processing user ${user.userId} with kycId ${user.kycId}...`,);
-
-    // Add worker job
-    await addKraWorkerJob(
-      {
-        customerId: user.userId,
-        kycDataStoreId: user.kycId,
-        stage: "MODIFY_KRA",
-      },
-      5000,
-    );
-
-    // Optional delay (if actually needed)
-    await delay(3000);
-
-    console.log(
-      `Added job for user ${user.userId} with kycId ${user.kycId}`,
-    );
-
-    console.log("All jobs added successfully");
+    const res = await participant.getAllParticipants();
+    const cbricsManager = new NseCBRICS();
+    await cbricsManager.unregisteredParticipant({
+      "loginId": "P0131B",
+      "firstName": "PART 131",
+      "panNo": "PANNO0131A",
+      "custodian": null,
+      "contactPerson": "Mr Test",
+      "mobileList": ["1231231231"],
+      "emailList": ["test@testcompany.com"],
+      "telephone": "1231231231",
+      "address": "Test line 1",
+      "address2": "Test line 2",
+      "address3": "Test line 3",
+      "stateCode": "16",
+      "regAddress": "Test registered address",
+      "leiCode": null,
+      "expiryDate": null,
+      "bankAccountList": [
+        {
+          "status": "A",
+          "bankName": "HDFC",
+          "bankIFSC": "HDFC1231221",
+          "bankAccountNo": "123412312",
+          "isDefault": "Y"
+        }
+      ],
+      "dpAccountList": [
+        {
+          "status": "A",
+          "dpType": "NSDL",
+          "dpId": "IN234234",
+          "benId": "29347239",
+          "isDefault": "Y"
+        }
+      ],
+      "dobDoi": "17-12-1996",
+    });
+    console.log(res);
   } catch (error) {
-    console.error("Failed to add KRA job:", error);
+    console.log((error as AxiosError)?.response?.data);
   }
 };
 
