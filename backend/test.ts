b27b4a51e1d965a22ddc502ac0a0401ad9140d0f
Sourav Bapari <hey.sourav.work@gmail.com>
2026-03-18 17:21:49 +0530

    Update KYC service and backend dependencies
    - Removed outdated test code and replaced it with a new implementation in CustomerKycKycService to create KRA verification requests.
    - Added express-http-proxy dependency to enhance proxy routing capabilities in the backend.
    - Updated package-lock.json and package.json to reflect the new dependencies and their versions.
    - Cleaned up server initialization by integrating proxy routes for improved request handling.
    - Deleted an unused Excel file related to API documentation.

--- backend/test.ts
+++ backend/test.ts
@@ -1,35 +1,9 @@
-import { addKraWorkerJob } from "@jobs/kra_worker/kraWroker.helper";
-import { cacheStorage } from "@store/redis_store";
-
-const user = {
-  "userId": 92,
-  "kycId": 617,
-};
-
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import { CustomerKycKycService } from "@resource/customer/kyc/kyc_process/customer_kyc.service";
 
 const main = async () => {
-  const cachedKey = `KRA:${user.userId}-${user.kycId}`;
-  const pastExecution = "MODIFY"; // MODIFY , REGISTER
-
-  await cacheStorage.delete(cachedKey).then(async () => {
-    if (pastExecution && pastExecution !== "") {
-      await cacheStorage.set(cachedKey, pastExecution, 72 * 60 * 60);
-    }
-
-    console.log("Processing user ${user.userId} with kycId ${user.kycId}...");
-    await addKraWorkerJob(
-      {
-        customerId: user.userId,
-        kycDataStoreId: user.kycId,
-        stage: "ENQUIRY_KRA",
-      },
-      5000,
-    );
-    await delay(3000);
-    console.log(`Added job for user ${user.userId} with kycId ${user.kycId}`);
-  });
-  console.log("All jobs added successfully");
+  const customerKycService = new CustomerKycKycService();
+  const response = await customerKycService.createKraVerifyRequestMock(3, { "pan": "AVEPK6139M", "dob": "30-05-1983" });
+  console.log(JSON.stringify(response));
 };
 
 main();
