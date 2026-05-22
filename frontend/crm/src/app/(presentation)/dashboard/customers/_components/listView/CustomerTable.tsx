935fb064a5211c4f1987fbe84026e758099ec2cc
SSugandhan <sugandhan@meradhan.co>
2026-05-22 16:16:12 +0530

    re-enable KRA status column in customer table.

--- frontend/crm/src/app/(presentation)/dashboard/customers/_components/listView/CustomerTable.tsx
+++ frontend/crm/src/app/(presentation)/dashboard/customers/_components/listView/CustomerTable.tsx
@@ -60,15 +60,13 @@ function CustomerTable({ data, pageSize = 10, isLoading }: UsersTableProps) {
           label: "KYC",
           cell: (row) => <StatusBadge value={row.kycStatus} />,
         },
-        // Commented temporarily to hide the KRA status column for Users.
-        // TODO: Uncomment when KRA status need 
-        // {
-        //   key: "kraStatus",
-        //   label: "KRA",
-        //   cell: (row) => (
-        //     <StatusBadge value={row.kraStatus || "Not Started"} />
-        //   ),
-        // },
+        {
+          key: "kraStatus",
+          label: "KRA",
+          cell: (row) => (
+            <StatusBadge value={row.kraStatus || "Not Started"} />
+          ),
+        },
         {
           key: "status",
           label: "Status",
